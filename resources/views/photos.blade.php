f12bfe17b9f8cca184b4de163392866b64893df0
haruna.nishi <haruna.nishi@rakuten.com>
2018-06-27 07:31:15 +0000

    photos design

--- resources/views/photos.blade.php
+++ resources/views/photos.blade.php
@@ -26,45 +26,14 @@
 @endsection
 
 @section('content')
-{!! Form::open(['url' => '/upload', 'method' => 'post', 'files' => true]) !!}
-
-{{--成功時のメッセージ--}}
-@if (session('success'))
-<div class="alert alert-success">{{ session('success') }}</div>
-@endif
-{{-- エラーメッセージ --}}
-@if ($errors->any())
-    <div class="alert alert-danger">
+<div class='container'>
     <ul>
-        @foreach ($errors->all() as $error)
-            <li>{{ $error }}</li>
-        @endforeach
-    </ul>
-    </div>
-@endif
+        
+       <li><img class="logo" src="{{ secure_asset('images/negi.jpg') }}" alt="negi"></li>
+</ul>
 
-<div class="form-group">
-    @if ($user->avatar_filename)
-        <p>
-            <img src="{{ asset('storage/avatar/' . $user->avatar_filename) }}" alt="avatar" />
-        </p>
-    @endif
-    {!! Form::label('file', '画像アップロード', ['class' => 'control-label']) !!}
-    {!! Form::file('file') !!}
+
+
 </div>
-
-<div class="form-group">
-    {!! Form::submit('アップロード', ['class' => 'btn btn-default']) !!}
-</div>
-{!! Form::close() !!}
-
-
-
-
-
-
-
-
-
 </div>
 @endsection
