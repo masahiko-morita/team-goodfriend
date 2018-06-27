8a13874c93d95ec7e6654421e605a184285c2b9c
haruna.nishi <haruna.nishi@rakuten.com>
2018-06-27 01:27:42 +0000

    make history page

---
+++ resources/views/history.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('cover')
+
+<div class="row">
+        <aside class="col-md-6 col-md-offset-6">
+            いいとも
+        </aside>
+</div>
+
+@endsection
+
+@section('content')
+
+@endsection
