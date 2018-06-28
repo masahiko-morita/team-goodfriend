b61bf1966e9eb91915aa40e7614094182687fd60
daiki.ishibashi <daiki.a.ishibashi@rakuten.com>
2018-06-28 07:30:04 +0000

    insert-philosophy

--- resources/views/rinen.blade.php
+++ resources/views/rinen.blade.php
@@ -1,16 +1,32 @@
 @extends('layouts.app')
 
 @section('cover')
+
 <div class='container'>
         <span class="text-center">
-         <h1>企業理念</h1>
+         <h1>Philosophy</h1>
         </span>
-        <img src="{{ secure_asset("images/iitomologo.png") }}" width =120% alt="いいともlogo">
-</div>
+
+
+    <div class='philosophy-content'>
+        <div class='philosophy-text'>
+            <h3>【Philosophy】</h3>
+            <h4>「21世紀を代表する会社を創る」</h4>
+            <h5>大切なこと、大切にします。<br>いいとも</h5> 
+            
+            <h3>【Mission Statement】</h3>
+            <h5>いいともは、<br>社会にとって 何が大切かを考え、行動します。<br>
+            地球にとって 何か大切かを考え、行動します。
+            <br>あなたにとって何が大切かを考え、行動します。</h5>
+        </div>
+    </div>
+
+</div>    
+
+
 @endsection
 
 @section('content')
 
-aiueo
 
 @endcontent
